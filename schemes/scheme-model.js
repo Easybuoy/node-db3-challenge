13c25013b93f6b1beee43d3a1bfffea10c8a79c0
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-18 12:57:34 +0100

    add findsteps method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -8,6 +8,13 @@ const findById = async id => {
   return await db("schemes").where({ id });
 };
 
+findSteps = async id => {
+  return await db("schemes")
+    .select("schemes.id", "scheme_name", "step_number", "instructions")
+    .join("steps")
+    .where({ scheme_id: id });
+};
+
 const add = async car => {
   return await db("schemes").insert(car);
 };
@@ -18,16 +25,17 @@ const remove = async id => {
     .delete();
 };
 
-function update(updatedCar, id ) {
-    return db('schemes')
-      .where({ id })
-      .update(updatedCar);
-  }
+function update(updatedCar, id) {
+  return db("schemes")
+    .where({ id })
+    .update(updatedCar);
+}
 
 module.exports = {
   find,
   findById,
   add,
   remove,
-  update
+  update,
+  findSteps
 };
